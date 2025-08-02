d4ff885b49f5c02e4abe5ca7e62d459b17adb4e1
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-08-02 09:59:29 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,52 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int idCounter = 1;
+    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        log.info("Получен запрос на получение всех фильмов. Текущее количество: {}", films.size());
+        return films.values();
+    }
+
+    @PostMapping
+    public Film createFilm(@Valid @RequestBody Film film) {
+        validateFilm(film);
+        film.setId(idCounter++);
+        films.put(film.getId(), film);
+        log.info("Создан новый фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            log.error("Фильм с id {} не найден", film.getId());
+            throw new ValidationException("Фильм не найден");
+        }
+        validateFilm(film);
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм: {}", film);
+        return film;
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
+            log.warn("Дата релиза {} раньше допустимой даты {}", film.getReleaseDate(), MIN_RELEASE_DATE);
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,71 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class FilmorateApplicationTests {
+	@Autowired
+	private FilmController filmController;
+
+	@Autowired
+	private UserController userController;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void createValidFilm() {
+		Film film = new Film();
+		film.setName("Тестовый фильм");
+		film.setDescription("Тестовое описание");
+		film.setReleaseDate(LocalDate.of(2000, 1, 1));
+		film.setDuration(120);
+
+		Film created = filmController.createFilm(film);
+		assertNotNull(created.getId());
+	}
+
+	@Test
+	void createInvalidFilm() {
+		Film film = new Film();
+		film.setName("");
+		film.setDescription("Очень длинное описание ".repeat(20));
+		film.setReleaseDate(LocalDate.of(1800, 1, 1));
+		film.setDuration(-1);
+
+		assertThrows(ValidationException.class, () -> filmController.createFilm(film));
+	}
+
+	@Test
+	void createValidUser() {
+		User user = new User();
+		user.setEmail("test@example.com");
+		user.setLogin("testlogin");
+		user.setBirthday(LocalDate.of(1990, 1, 1));
+
+		User created = userController.createUser(user);
+		assertNotNull(created.getId());
+	}
+
+	@Test
+	void createInvalidUser() {
+		User user = new User();
+		user.setEmail("неправильный-email");
+		user.setLogin("");
+		user.setBirthday(LocalDate.now().plusDays(1));
+
+		assertThrows(ValidationException.class, () -> userController.createUser(user));
+	}
 }
